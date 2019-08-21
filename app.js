65d852fda52da30ba60c5a80f3ed004a56ee2143
Scott Metoyer <scott.metoyer@gmail.com>
2019-08-21 11:52:13 -0700

    Added midi message transmit

--- app.js
+++ app.js
@@ -20,9 +20,36 @@ const input = readline.createInterface({
   output: process.stdout
 });
 
+function notesOff(note1, note2) {
+  output.sendMessage([144, note1, 0]);
+  output.sendMessage([145, note2, 0]);
+}
+
 input.on('line', (input) =>{
   console.log('Received: ' + input);
+
+  // Parse the input into midi channel, note, pressure
+  // 144, 145 are channel 1 and 2 out respectively
+  // Tweak the max out and in values to map the usable range of the parameters
+  var byte1 = Math.round(scale(input.substr(1, 2), 0, 100, 40, 80));
+  var byte2 = Math.round(scale(input.substr(3, 2), 0, 100, 40, 80));
+  var byte3 = Math.round(scale(input.substr(5, 2), 0, 100, 0, 127));
+  var byte4 = Math.round(scale(input.substr(7, 2), 0, 100, 0, 127));
+  // var byte5 = Math.round(scale(input.substr(9, 2), 0, 100, 0, 127));
+
+  // Send the MIDI data
+  console.log('Parsed MIDI values: ' + byte1 + ' ' + byte2 + ' ' + byte3 + ' ' + byte4);
+
+  output.sendMessage([144, byte1, byte3])
+  output.sendMessage([145, byte2, byte4])
+
+  // Turn the note off after half a second.
+  setTimeout(notesOff, 250, byte1, byte2);
 });
 
 // output.sendMessage([176,22,1]);
 // output.closePort();
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
