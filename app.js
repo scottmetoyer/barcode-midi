5ef8204da6862fe044b392d5247a5d6d282b54ef
Scott Metoyer <scott.metoyer@gmail.com>
2019-09-08 09:04:58 -0700

    Switched to CC values 33 and 34 for Disting compatibility

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ for (var i = 0; i < portCount; i++) {
   console.log(i + ': ' + output.getPortName(i));
 }
 
-var deviceNumber = 1;
+var deviceNumber = 2;
 output.openPort(deviceNumber);
 
-console.log('Port opened. Waiting for input.');
+console.log('Opened ' + output.getPortName(deviceNumber) +  '. Waiting for input.');
 
 const input = readline.createInterface({
   input: process.stdin,
@@ -31,20 +31,20 @@ input.on('line', (input) =>{
   // Parse the input into midi channel, note, pressure
   // 144, 145 are channel 1 and 2 out respectively
   // Tweak the max out and in values to map the usable range of the parameters
-  var byte1 = Math.round(scale(input.substr(1, 2), 0, 100, 40, 80));
-  var byte2 = Math.round(scale(input.substr(3, 2), 0, 100, 40, 80));
-  var byte3 = Math.round(scale(input.substr(5, 2), 0, 100, 0, 127));
-  var byte4 = Math.round(scale(input.substr(7, 2), 0, 100, 0, 127));
+  var byte1 = Math.round(scale(input.substr(2, 2), 0, 100, 0, 127));
+  var byte2 = Math.round(scale(input.substr(4, 2), 0, 100, 0, 127));
+  // var byte3 = Math.round(scale(input.substr(5, 2), 0, 100, 0, 127));
+  // var byte4 = Math.round(scale(input.substr(7, 2), 0, 100, 0, 127));
   // var byte5 = Math.round(scale(input.substr(9, 2), 0, 100, 0, 127));
 
   // Send the MIDI data
-  console.log('Parsed MIDI values: ' + byte1 + ' ' + byte2 + ' ' + byte3 + ' ' + byte4);
+  console.log('Parsed MIDI values: ' + byte1 + ' ' + byte2);
 
-  output.sendMessage([144, byte1, byte3])
-  output.sendMessage([145, byte2, byte4])
+  output.sendMessage([176, 33, byte1]);
+  output.sendMessage([176, 34, byte2]);
 
   // Turn the note off after half a second.
-  setTimeout(notesOff, 250, byte1, byte2);
+  // setTimeout(notesOff, 250, byte1, byte2);
 });
 
 // output.sendMessage([176,22,1]);
